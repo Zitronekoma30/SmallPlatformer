5e7763f8068660b1ab9ff2faf05002434e0faa0c
Leon <leon.andrassik@gmail.com>
2023-03-23 09:57:53 +0100

    implemented teleporter functionality

--- Objects/teleporter.gd
+++ Objects/teleporter.gd
@@ -1,6 +1,8 @@
 extends Area2D
 
-@onready var sprite = $Sprite2D
+@export var destination: Marker2D
+
+@onready var sprite := $Sprite2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -10,3 +12,12 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	sprite.rotation += 0.1
+
+func teleport(player: CharacterBody2D) -> void:
+	player.global_position = destination.global_position
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"): body.interact.connect(teleport)
+
+func _on_body_exited(body: Node2D) -> void:
+	if body.is_in_group("player"): body.interact.disconnect(teleport)
